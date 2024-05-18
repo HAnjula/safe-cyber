c6f632fa75f8d179170c187967df3f050ca924d4
Anjula handapangoda <handapangodageachinthana@gmail.com>
2024-05-18 11:55:45 +0530

    Add some styles

--- src/components/Ransomware.jsx
+++ src/components/Ransomware.jsx
@@ -150,18 +150,21 @@ const Ransomware = () => {
                     <h2>Password Strength Checker</h2>
                     <form onSubmit={handleSubmit}>
                       <input
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         placeholder="Enter your password"
                         required
                       />
-                      <button type="submit">Check Strength</button>
+                      <button
+                        type="submit"
+                        className=" text-color-3 font-semibold py-2 px-4"
+                      >
+                        Check strength
+                      </button>
                     </form>
-                    {result && <p>{result}</p>}
-                    <button className="hover:bg-gray-800 text-color-3 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-                      Live Demo
-                    </button>
+                    {result && <p className="text-center">{result}</p>}
                   </>
                 )}
               </div>
