582d507ce93393d402cc46e881c4fa914771a0d4
Anjula handapangoda <handapangodageachinthana@gmail.com>
2024-06-23 17:34:51 +0530

    register the user added

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,7 +1,39 @@
 import { technology1 } from "../assets";
 import { Link } from "react-router-dom";
-
+import axios from "axios";
+import { useState } from "react";
 const Register = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:3000/api/user/sign-up",
+        formData
+      );
+      console.log("User signed up successfully:", response.data);
+    } catch (error) {
+      console.error(
+        "Failed to sign up user:",
+        error.response?.data || error.message
+      );
+    }
+  };
   return (
     <div className="relative min-w-screen min-h-screen bg-purple-900 flex items-center justify-center px-5 py-5 overflow-hidden">
       <video
@@ -26,106 +58,119 @@ const Register = () => {
                   Enter your information to register
                 </p>
               </div>
-              <div>
-                <div className="flex -mx-3">
-                  <div className="w-1/2 px-3 mb-5">
-                    <label
-                      htmlFor="firstName"
-                      className="text-xs font-semibold px-1 text-gray-200"
-                    >
-                      First name
-                    </label>
-                    <div className="flex">
-                      <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-                        <i className="mdi mdi-account-outline text-gray-300 text-lg"></i>
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <div className="flex -mx-3">
+                    <div className="w-1/2 px-3 mb-5">
+                      <label
+                        htmlFor="firstName"
+                        className="text-xs font-semibold px-1 text-gray-200"
+                      >
+                        First name
+                      </label>
+                      <div className="flex">
+                        <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                          <i className="mdi mdi-account-outline text-gray-300 text-lg"></i>
+                        </div>
+                        <input
+                          type="text"
+                          id="firstName"
+                          className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-purple-200 outline-none focus:border-purple-600"
+                          placeholder="John"
+                          value={formData.firstName}
+                          onChange={handleChange}
+                        />
                       </div>
-                      <input
-                        type="text"
-                        id="firstName"
-                        className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-purple-200 outline-none focus:border-purple-600"
-                        placeholder="John"
-                      />
+                    </div>
+                    <div className="w-1/2 px-3 mb-5">
+                      <label
+                        htmlFor="lastName"
+                        className="text-xs font-semibold px-1 text-gray-200"
+                      >
+                        Last name
+                      </label>
+                      <div className="flex">
+                        <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                          <i className="mdi mdi-account-outline text-gray-400 text-lg"></i>
+                        </div>
+                        <input
+                          type="text"
+                          id="lastName"
+                          className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-purple-600"
+                          placeholder="Smith"
+                          value={formData.lastName}
+                          onChange={handleChange}
+                        />
+                      </div>
                     </div>
                   </div>
-                  <div className="w-1/2 px-3 mb-5">
-                    <label
-                      htmlFor="lastName"
-                      className="text-xs font-semibold px-1 text-gray-200"
-                    >
-                      Last name
-                    </label>
-                    <div className="flex">
-                      <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-                        <i className="mdi mdi-account-outline text-gray-400 text-lg"></i>
+                  <div className="flex -mx-3">
+                    <div className="w-full px-3 mb-5">
+                      <label
+                        htmlFor="email"
+                        className="text-xs font-semibold px-1 text-gray-200"
+                      >
+                        Email
+                      </label>
+                      <div className="flex">
+                        <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                          <i className="mdi mdi-email-outline text-gray-400 text-lg"></i>
+                        </div>
+                        <input
+                          type="email"
+                          id="email"
+                          className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-purple-600"
+                          placeholder="johnsmith@example.com"
+                          value={formData.email}
+                          onChange={handleChange}
+                        />
                       </div>
-                      <input
-                        type="text"
-                        id="lastName"
-                        className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-purple-600"
-                        placeholder="Smith"
-                      />
                     </div>
                   </div>
-                </div>
-                <div className="flex -mx-3">
-                  <div className="w-full px-3 mb-5">
-                    <label
-                      htmlFor="email"
-                      className="text-xs font-semibold px-1 text-gray-200"
-                    >
-                      Email
-                    </label>
-                    <div className="flex">
-                      <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-                        <i className="mdi mdi-email-outline text-gray-400 text-lg"></i>
+                  <div className="flex -mx-3">
+                    <div className="w-full px-3 mb-12">
+                      <label
+                        htmlFor="password"
+                        className="text-xs font-semibold px-1 text-gray-200"
+                      >
+                        Password
+                      </label>
+                      <div className="flex">
+                        <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
+                          <i className="mdi mdi-lock-outline text-gray-400 text-lg"></i>
+                        </div>
+                        <input
+                          type="password"
+                          id="password"
+                          className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-purple-600"
+                          placeholder="************"
+                          value={formData.password}
+                          onChange={handleChange}
+                        />
                       </div>
-                      <input
-                        type="email"
-                        id="email"
-                        className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-purple-600"
-                        placeholder="johnsmith@example.com"
-                      />
                     </div>
                   </div>
-                </div>
-                <div className="flex -mx-3">
-                  <div className="w-full px-3 mb-12">
-                    <label
-                      htmlFor="password"
-                      className="text-xs font-semibold px-1 text-gray-200"
-                    >
-                      Password
-                    </label>
-                    <div className="flex">
-                      <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center">
-                        <i className="mdi mdi-lock-outline text-gray-400 text-lg"></i>
-                      </div>
-                      <input
-                        type="password"
-                        id="password"
-                        className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-purple-600"
-                        placeholder="************"
-                      />
+                  <div className="flex -mx-3">
+                    <div className="w-full px-3 mb-5">
+                      <button
+                        type="submit"
+                        className="block w-full max-w-xs mx-auto bg-purple-500 hover:bg-purple-700 focus:bg-purple-700 text-white rounded-lg px-3 py-3 font-semibold"
+                      >
+                        REGISTER NOW
+                      </button>
                     </div>
                   </div>
-                </div>
-                <div className="flex -mx-3">
-                  <div className="w-full px-3 mb-5">
-                    <button className="block w-full max-w-xs mx-auto bg-purple-500 hover:bg-purple-700 focus:bg-purple-700 text-white rounded-lg px-3 py-3 font-semibold">
-                      REGISTER NOW
-                    </button>
+                  <div className="text-center">
+                    <Link
+                      to={"/login"}
+                      className="inline-block text-sm text-purple-400 align-baseline hover:text-purple-300"
+                      href="#"
+                    >
+                      Already have an account? Login!
+                    </Link>
                   </div>
                 </div>
-                <div className="text-center">
-                  <Link
-                    to={"/login"}
-                    className="inline-block text-sm text-purple-400 align-baseline hover:text-purple-300"
-                    href="#"
-                  >
-                    Already have an account? Login!
-                  </Link>
-                </div>
-              </div>
+              </form>
             </div>
           </div>
         </div>
