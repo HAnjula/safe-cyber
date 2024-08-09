931322cce2dff036ff84f806710e65439f3bc294
Anjula handapangoda <handapangodageachinthana@gmail.com>
2024-08-09 10:46:47 +0530

    news updated

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Section from "./Section";
 
 const API_KEY = "ad409a5f75574ea38cb418fb049d7231";
 const API_URL = `https://newsapi.org/v2/everything?q=cyber+attack&apiKey=${API_KEY}`;
@@ -37,32 +38,34 @@ const CyberAttackNews = () => {
   }
 
   return (
-    <div className="p-5 max-w-3xl mx-auto font-sans bg-gray-100 rounded-lg shadow-lg">
-      <h2 className="text-2xl text-center text-gray-800 mb-6">
-        Latest Cyber Attack News
-      </h2>
-      <ul className="space-y-6">
-        {news.map((article, index) => (
-          <li key={index} className="border-b border-gray-300 pb-6">
-            <h3 className="text-xl text-blue-600 hover:text-blue-800">
-              {article.title}
-            </h3>
-            <p className="text-gray-700 mt-2">{article.description}</p>
-            <a
-              href={article.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline mt-2 block"
-            >
-              Read more
-            </a>
-            <time className="text-gray-500 text-sm mt-2 block">
-              Published on: {new Date(article.publishedAt).toLocaleString()}
-            </time>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Section crosses>
+      <div className="p-5 max-w-3xl mx-auto bg-n-8 font-sans bg-gray-100 rounded-lg shadow-lg">
+        <h2 className="text-2xl text-center text-gray-800 mb-6">
+          Latest Cyber Attack News
+        </h2>
+        <ul className="space-y-6">
+          {news.map((article, index) => (
+            <li key={index} className="border-b border-gray-300 pb-6">
+              <h3 className="text-xl text-blue-600 hover:text-blue-800">
+                {article.title}
+              </h3>
+              <p className="text-gray-700 mt-2">{article.description}</p>
+              <a
+                href={article.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline mt-2 block"
+              >
+                Read more
+              </a>
+              <time className="text-gray-500 text-sm mt-2 block">
+                Published on: {new Date(article.publishedAt).toLocaleString()}
+              </time>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Section>
   );
 };
 
